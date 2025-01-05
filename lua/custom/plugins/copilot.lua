fee4bae1bc6ef55b1ec6b280291dfef73aa5d07c
Rui Caridade <ruidanielcaridade@gmail.com>
2025-01-05 03:24:10 +0000

    add copilot chat

--- lua/custom/plugins/copilot.lua
+++ lua/custom/plugins/copilot.lua
@@ -3,4 +3,31 @@ return {
     lazy = false,
     'github/copilot.vim',
   },
+  {
+    'CopilotC-Nvim/CopilotChat.nvim',
+    dependencies = {
+      { 'github/copilot.vim' },
+      { 'nvim-lua/plenary.nvim', branch = 'master' },
+    },
+    build = 'make tiktoken',
+    keys = {
+      {
+        '<leader>cct',
+        function()
+          require('CopilotChat').toggle()
+        end,
+        mode = '',
+        desc = '[C]opilot [C]hat [T]oggle',
+      },
+      {
+        '<leader>ccr',
+        function()
+          require('CopilotChat').reset()
+        end,
+        mode = '',
+        desc = '[C]opilot [C]hat [R]eset',
+      },
+    },
+    opts = {},
+  },
 }
