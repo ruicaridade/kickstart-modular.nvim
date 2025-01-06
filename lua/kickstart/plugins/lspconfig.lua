405d8b0d35508a0f76db170cd490bab3f601a72f
Rui Caridade <rui@lineupnow.com>
2025-01-06 11:36:44 +0000

    feat: configure eslint and ensure mypy is installed

--- lua/kickstart/plugins/lspconfig.lua
+++ lua/kickstart/plugins/lspconfig.lua
@@ -263,6 +263,7 @@ return {
       local ensure_installed = vim.tbl_keys(servers or {})
       vim.list_extend(ensure_installed, {
         'stylua', -- Used to format Lua code
+        'mypy',
       })
       require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
