94dd6b3a3c05f3431380de5994823caf90abc688
Rui Caridade <rui@lineupnow.com>
2025-01-10 13:48:13 +0000

    feat: add omnisharp

--- lua/kickstart/plugins/lspconfig.lua
+++ lua/kickstart/plugins/lspconfig.lua
@@ -221,6 +221,7 @@ return {
             },
           },
         },
+        omnisharp = {},
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
