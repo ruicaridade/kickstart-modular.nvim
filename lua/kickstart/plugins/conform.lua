cff57aa247ae6c4a87f3d3739861e8803b7fad84
Rui Caridade <rui@lineupnow.com>
2025-01-10 14:05:55 +0000

    feat: add csharpier as c# formatter

--- lua/kickstart/plugins/conform.lua
+++ lua/kickstart/plugins/conform.lua
@@ -41,6 +41,7 @@ return {
         typescriptreact = { 'prettierd', 'prettier', stop_after_first = true },
         json = { 'prettier' },
         python = { 'ruff_format', 'ruff_organize_imports' },
+        cs = { 'csharpier' },
       },
     },
   },
