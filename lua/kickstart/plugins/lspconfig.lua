9a9a96263e4f1dab520389ff8021b927bc33dcc8
Rui Caridade <ruidanielcaridade@gmail.com>
2025-02-12 00:50:40 +0000

    feat: add astro

--- lua/kickstart/plugins/lspconfig.lua
+++ lua/kickstart/plugins/lspconfig.lua
@@ -197,6 +197,7 @@ return {
           },
         },
         rust_analyzer = {},
+        astro = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
