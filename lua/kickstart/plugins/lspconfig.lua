0f18a2d66dbf3e325d90c0525dfa61ae0bc6bd99
Rui Caridade <ruidanielcaridade@gmail.com>
2025-02-22 03:02:12 +0000

    feat: add ruby and prisma lsp

--- lua/kickstart/plugins/lspconfig.lua
+++ lua/kickstart/plugins/lspconfig.lua
@@ -231,6 +231,8 @@ return {
         },
         rust_analyzer = {},
         astro = {},
+        ruby_lsp = {},
+        prismals = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
