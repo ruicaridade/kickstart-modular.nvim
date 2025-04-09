aa25794e6b5c7392cef9e0de1f059bcbdd966c2f
Rui Caridade <rui@lineupnow.com>
2025-04-09 10:04:51 +0100

    feat: change theme to rose pine

--- lua/custom/plugins/theme.lua
+++ lua/custom/plugins/theme.lua
@@ -1,9 +1,18 @@
 return {
+  -- {
+  --   'catppuccin/nvim',
+  --   priority = 1000,
+  --   config = function()
+  --     vim.cmd.colorscheme 'catppuccin-mocha'
+  --     vim.cmd.hi 'Comment gui=none'
+  --   end,
+  -- },
   {
-    'catppuccin/nvim',
+    'rose-pine/neovim',
+    name = 'rose-pine',
     priority = 1000,
     config = function()
-      vim.cmd.colorscheme 'catppuccin-mocha'
+      vim.cmd.colorscheme 'rose-pine'
       vim.cmd.hi 'Comment gui=none'
     end,
   },
