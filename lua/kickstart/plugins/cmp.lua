f717bdc62c606ba1065c1249d3fe2a99773341d8
Rui Caridade <rui@lineupnow.com>
2025-05-02 13:16:59 +0100

    feat: restore previous cmp formatting

--- lua/kickstart/plugins/cmp.lua
+++ lua/kickstart/plugins/cmp.lua
@@ -52,17 +52,6 @@ return {
         },
         completion = { completeopt = 'menu,menuone,noinsert' },
 
-        formatting = {
-          fields = { 'kind', 'abbr', 'menu' },
-          format = function(entry, vim_item)
-            local kind = require('lspkind').cmp_format { mode = 'symbol_text', maxwidth = 50 }(entry, vim_item)
-            local strings = vim.split(kind.kind, '%s', { trimempty = true })
-            kind.kind = ' ' .. (strings[1] or '') .. ' '
-            kind.menu = '    (' .. (strings[2] or '') .. ')'
-            return kind
-          end,
-        },
-
         -- For an understanding of why these mappings were
         -- chosen, you will need to read `:help ins-completion`
         --
