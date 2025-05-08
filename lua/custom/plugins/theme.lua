c18a28f36de5ff443f939fdb2ffe8ffd09f004f3
Rui Caridade <rui@lineupnow.com>
2025-05-08 14:15:54 +0100

    feat: disable italics in rose-pine

--- lua/custom/plugins/theme.lua
+++ lua/custom/plugins/theme.lua
@@ -12,6 +12,12 @@ return {
     name = 'rose-pine',
     priority = 1000,
     config = function()
+      require('rose-pine').setup {
+        styles = {
+          italic = false,
+        },
+      }
+
       vim.cmd.colorscheme 'rose-pine'
       vim.cmd.hi 'Comment gui=none'
     end,
