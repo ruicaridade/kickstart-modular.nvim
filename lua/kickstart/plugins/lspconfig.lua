a7cc345ab389c3bee9d0ee7d93338e5d0f7557ac
Rui Caridade <rui@lineupnow.com>
2025-05-08 11:55:00 +0100

    feat: add helm_ls

--- lua/kickstart/plugins/lspconfig.lua
+++ lua/kickstart/plugins/lspconfig.lua
@@ -249,6 +249,7 @@ return {
           },
         },
         phpactor = {},
+        helm_ls = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
