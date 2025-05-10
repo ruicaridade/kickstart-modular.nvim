b3767ad1f81682f0ea3a42da52977633db35f299
Rui Caridade <ruidanielcaridade@gmail.com>
2025-05-10 11:13:18 +0100

    feat: adjust nvim-cmp comparators

--- lua/kickstart/plugins/cmp.lua
+++ lua/kickstart/plugins/cmp.lua
@@ -112,7 +112,49 @@ return {
           },
           { name = 'nvim_lsp' },
           { name = 'luasnip' },
+          { name = 'copilot' },
           { name = 'path' },
+        },
+
+        sorting = {
+          priority_weight = 1.0,
+
+          comparators = {
+            cmp.config.compare.offset,
+            cmp.config.compare.exact,
+            cmp.config.compare.score,
+
+            -- Give underscores a lower priority
+            function(entry1, entry2)
+              local _, entry1_under = entry1.completion_item.label:find '^_+'
+              local _, entry2_under = entry2.completion_item.label:find '^_+'
+              entry1_under = entry1_under or 0
+              entry2_under = entry2_under or 0
+              if entry1_under > entry2_under then
+                return false
+              elseif entry1_under < entry2_under then
+                return true
+              end
+            end,
+
+            -- If variable ends with "=", give it a higher priority
+            function(entry1, entry2)
+              local _, entry1_eq = entry1.completion_item.label:find '=$'
+              local _, entry2_eq = entry2.completion_item.label:find '=$'
+              entry1_eq = entry1_eq or 0
+              entry2_eq = entry2_eq or 0
+              if entry1_eq > entry2_eq then
+                return true
+              elseif entry1_eq < entry2_eq then
+                return false
+              end
+            end,
+
+            cmp.config.compare.kind,
+            cmp.config.compare.sort_text,
+            cmp.config.compare.length,
+            cmp.config.compare.order,
+          },
         },
       }
     end,
