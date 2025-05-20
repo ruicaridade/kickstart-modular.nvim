7bfe4cb92f0009510eecdf0371ebc3201d7c2816
Rui Caridade <rui@lineupnow.com>
2025-05-20 12:52:30 +0100

    feat: change to rosebones

--- lua/custom/plugins/theme.lua
+++ lua/custom/plugins/theme.lua
@@ -21,7 +21,7 @@ return {
     priority = 1000,
     config = function()
       vim.g.zenbones_darken_comments = 45
-      vim.cmd.colorscheme 'zenbones'
+      vim.cmd.colorscheme 'rosebones'
     end,
   },
 }
